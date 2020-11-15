550fde79ddd42b1308780c0368f629119f36a3e6
Ferams <oladosuferanmi@gmail.com>
2020-11-15 16:42:12 +0100

    Added GameManager.cs

---
+++ Assets/Scripts/Fertools/Common Managers/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Fertools.Utils;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+   
+   public void Pause()
+   {
+      print("Pause");
+      var tse = new TimeScaleEvent(0, 0 , false, 0, true);
+      tse.FireEvent();
+      
+   }
+
+   public void Resume()
+   {
+      var tse = new TimeScaleEvent(1, 0 , false, 0, true);
+      tse.FireEvent();
+      print("Resume");
+
+   }
+   
+   public void Restart()
+   {
+      var tse = new TimeScaleEvent(1, 0 , false, 0, true);
+      tse.FireEvent();
+      print("Restart");
+
+      LoadingScene.LoadScene(SceneManager.GetActiveScene().name);
+   }
+
+
+   public void QuitToMenu()
+   {
+      var tse = new TimeScaleEvent(1, 0 , false, 0, true);
+      tse.FireEvent();
+      print("Quit To MEnu");
+
+      LoadingScene.LoadScene(SceneNames.MENU);
+   }
+   
+}
+
